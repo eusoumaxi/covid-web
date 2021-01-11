b196bd5b623731ccd8e0eca8fd5c753ef037e83d
david <d99maxi@gmail.com>
2021-01-11 15:21:21 -0500

    finish change

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const execSync = require('child_process').execSync;
 
 //pm2 start --name "webNextJS" npm -- run start
 console.log('init')
+
+console.log('run checkout github')
 execSync('git pull origin main')
+console.log('finish checkout github')
+
+console.log('run build')
 execSync('npm run build')
+console.log('finih build')
+
+console.log('upload build')
 execSync('vercel --prod')
+console.log('finish upload build')
+
 console.log('finish')
