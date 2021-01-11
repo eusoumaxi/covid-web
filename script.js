2973c3d1aea5ffb8417fe26adfa7218a68dca39d
david <d99maxi@gmail.com>
2021-01-11 15:15:19 -0500

    fic script for sever

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync;
 
 //pm2 start --name "webNextJS" npm -- run start
 console.log('init')
+execSync('git pull origin master')
 execSync('npm run build')
-execSync('pm2 stop webNextJS')
-execSync('pm2 start webNextJS')
+execSync('vercel --prod')
 console.log('finish')
