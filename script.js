daa471049a619ab0a1a9d6717530b69bd4d39154
david <d99maxi@gmail.com>
2021-01-11 15:17:28 -0500

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync;
 
 //pm2 start --name "webNextJS" npm -- run start
 console.log('init')
-execSync('git pull origin master')
+execSync('git pull origin main')
 execSync('npm run build')
 execSync('vercel --prod')
 console.log('finish')
