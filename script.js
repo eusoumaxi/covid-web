db315370cea9a3bf11f92b9a303c52b9a8227618
david <d99maxi@gmail.com>
2021-01-11 19:39:28 -0500

    server

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const execSync = require('child_process').execSync;
 
 //pm2 start --name "webNextJS" npm -- run start
 console.log('init')
-
 console.log('run checkout github')
 execSync('git pull origin main')
 console.log('finish checkout github')
-
-// console.log('run build')
-// execSync('npm run build')
-// console.log('finih build')
-
+console.log('create')
+execSync('touch build.js')
 console.log('upload build')
 execSync('vercel --prod')
 console.log('finish upload build')
-
+console.log('delete build')
+execSync('rm build.js')
 console.log('finish')
